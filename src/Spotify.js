ba645cc4c351b47aa3b90794be2ab0ab16712be7
ErnestoJuniorFrwk <ernestojunior@frwk.com.br>
2023-03-02 09:19:32 -0300

    chore(initialized): start project

---
+++ src/Spotify.js
@@ -0,0 +1,7 @@
+import SpotifyWrapper from 'spotify-wrapper';
+
+const spotify = new SpotifyWrapper({
+  token: 'BQA1CkqEB1mBrrJ8kWFhuTEQRK6Hg8WP9xcvkWjBjAq7Ad6RYbYXB-PmlJgk90loZsZpPoJp5xkD6i0pHcFtuxjESKzpGGcbWcx8GInLCtylCY1iezUXhqTDq7VOPimyCJv0oYPXAaHX9r3wheO2jk1X9kyHS2yweemvZYkzx1a9XUWmg3MoGqgmizPP8jiBr9GGrqxMvIgsJL81ZCjcGPip3fh3Mrbr-7ULGOrY-kdQhNCDU_yYb7azSTpmH62J6YZVPitNNhDoPDEdVWQaqmh6uqYt7qRk1sIwOcG85bYeM5hcDl_9AcWATBbDzZTA-QE8iCHGBj-9mQ',
+});
+
+export default spotify;
